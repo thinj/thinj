3617a4b9efc50c1fe9996fe3d4380ab0be317afd
Henrik Jaeger <thinjaeger@gmail.com>
2012-07-06 19:04:40 +0200

    Ahhh, moved common dependcies into this file

---
+++ config.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+#----------------------------------------------
+# The version to build
+#----------------------------------------------
+VERSION=devel
+
+
+#----------------------------------------------
+# Select the target architecture of the thinjvm
+#----------------------------------------------
+export ARCH=
+#export ARCH=arm
+
+
+#
+# Setup miscellaneous parameters for build:
+#
+if [ "$ARCH" = "" ]; then
+    export CMD_PREFIX=
+    export ENV_PREFIX=
+    export GCC=gcc
+    export CFLAGS="-I. -Wall -c -pg"    
+    export NOSTDLIB=    
+    export AR=ar
+elif [ "$ARCH" = "arm" ]; then
+    export CMD_PREFIX=arm-
+    export ENV_PREFIX=ARM_
+    export GCC=arm-elf-gcc
+    export AR=arm-elf-ar
+    export CFLAGS="-I. -Wall -c \
+           -mcpu=arm7tdmi -mlittle-endian -gdwarf-2 -std=c99 -march=armv4t -mlittle-endian -fno-builtin"
+    export NOSTDLIB=nostdlib.o    
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+export TOOL_DIR=${CMD_PREFIX}thinj
+
+export DESTINATION=/tools/$TOOL_DIR/$VERSION
+export LIBDIR=$DESTINATION/lib
+export BINDIR=$DESTINATION/bin
+export INCDIR=$DESTINATION/inc
+
+#
+# Create tool directories:
+#
+mkdir -p $BINDIR || exit 1
+mkdir -p $LIBDIR || exit 1
+mkdir -p $INCDIR || exit 1
